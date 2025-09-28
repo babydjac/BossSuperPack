57bca888272ce480296edec221d06efa9330a8fa
babydjac <133718954+babydjac@users.noreply.github.com>
2025-09-28 00:19:52 -0700

    Add files via upload

---
+++ wan22_expert_prompter/__init__.py
@@ -0,0 +1,9 @@
+from .wan22_expert_prompter_node import WAN22ExpertPrompterNode
+
+NODE_CLASS_MAPPINGS = {
+    "WAN22ExpertPrompterNode": WAN22ExpertPrompterNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "WAN22ExpertPrompterNode": "WAN 2.2 Expert Prompter",
+}
